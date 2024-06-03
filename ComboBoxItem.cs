2e384f46cb5156af64c014c1f9c3e7c9477817ba
zGeorgi <65114815+zGeorgi@users.noreply.github.com>
2024-06-03 18:38:35 +0100

    Add files via upload

---
+++ ComboBoxItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject1
+{
+
+    public class ComboBoxItem<T>
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public T Value { get; set; }
+
+        public override string ToString()
+        {
+            return Text;
+        }
+    }
+}
